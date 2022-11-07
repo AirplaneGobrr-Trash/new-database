4aa89df796c22680672e9ccaba7f40163b6d6b36
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-11-07 10:25:25 -0800

    updates

--- database.js
+++ database.js
@@ -3,10 +3,53 @@ class dbClient {
     constructor(url, database, auth){
         this.socket = io(url, {
             auth: {
-                database,
-                auth
+                database: database,
+                auth: auth
             }
         });
+        this.cache = {}
+        this.defaultTable = null
+    }
+
+    async get(path) {
+        return new Promise((resolve, reject) => {
+            if (!path) path = this.defaultTable; else if (this.defaultTable) path = `${this.defaultTable}.${path}`;
+            this.socket.emit("get", path, (a)=>{
+                if (a && a?.error) reject(a)
+                return resolve(a)
+            })
+        })
+        
+    }
+
+    async set(path, value) {
+        return new Promise((resolve, reject) => {
+            if (this.defaultTable) path = `${this.defaultTable}.${path}`
+            this.socket.emit("set", path, value, (a)=>{
+                if (a && a?.error) reject(a)
+                resolve(a)
+            })
+        })
+    }
+
+    async has(path) {
+        return new Promise((resolve, reject) => {
+            if (this.defaultTable) path = `${this.defaultTable}.${path}`
+            this.socket.emit("has", path, (a)=>{
+                if (a && a?.error) reject(a)
+                resolve(a)
+            })
+        })
+    }
+
+    async push(path, value) {
+        return new Promise((resolve, reject) => {
+            if (this.defaultTable) path = `${this.defaultTable}.${path}`
+            this.socket.emit("push", path, value, (a)=>{
+                if (a && a?.error) reject(a)
+                resolve(a)
+            })
+        })
     }
 }
 
--- test.js
+++ test.js
@@ -1,2 +1,35 @@
 const dbClass = require("./database")
-const db = new dbClass("http://localhost:3000/", "test_db", "password")
+
+
+async function way1(){
+    const db = new dbClass("http://localhost:3500/", "test_db", "password")
+
+    // Init the table of the database
+    await db.set("servers", {})
+    // Now we can use it
+    await db.set("servers.723776438712664094", {
+        users: [793281470696652821, 904360748455698502, 250029754076495874]
+    })
+    // Set the default table to use (can be very usefull!)
+    db.defaultTable = "servers"
+    console.log(await db.get())
+    db.defaultTable = "servers.723776438712664094"
+    console.log(await db.get())
+}
+
+async function way2(){
+    const db = new dbClass("http://localhost:3500/", "test_db.servers", "password")
+    await db.set("723776438712664094", { users: [793281470696652821, 904360748455698502, 250029754076495874] })
+    db.defaultTable = "723776438712664094" // Make sure to always use strings!
+    console.log(await db.get())
+
+    console.log("error")
+    // Error example: 
+    try {// Make sure to always use strings! Javascript will convert this (723776438712664094) to 723776438712664000 (replaces 94 to 00)
+        db.defaultTable = 723776438712664094 
+        console.log(await db.get()) // ERROR! No database!
+    } catch (e) {
+        console.log(e)
+    }
+}
+way2()
